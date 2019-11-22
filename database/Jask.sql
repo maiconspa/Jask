ce7789262e5afc7dd44d67cfe5000c56b0705308
davibrandao18 <davi.brandao2013@gmail.com>
2019-11-22 19:05:23 -0300

    Adição do arquivo 2.0 do db

--- database/Jask.sql
+++ database/Jask.sql
@@ -6,28 +6,28 @@ create table Usuario(
 nome varchar(60) not null,
 email varchar(200) not null unique,
 telefone varchar(20),
-apelido varchar(20) primary key not null unique,
+apelido varchar(20) primary key unique,
 senha varchar(100) not null,
 foto longblob
 );
 
 create table Projeto(
-id_projeto serial,
+id_projeto int primary key unique,
 nome varchar(30),
 fk_apelido_proprietario varchar(20),
 foreign key (fk_apelido_proprietario) references Usuario(apelido)
 );
 
 create table Item_usuario_projeto(
-id_item_usuario_projeto serial,
+id_item_usuario_projeto int primary key unique,
 fk_apelido_usuario varchar(20),
-fk_id_projeto bigint,
+fk_id_projeto int,
 foreign key (fk_apelido_usuario) references Usuario(apelido),
 foreign key (fk_id_projeto) references Projeto(id_projeto)
 );
 
 create table Mensagem(
-id_mensagem serial,
+id_mensagem int primary key unique,
 fk_apelido_remetente varchar(20),
 fk_apelido_destinatario varchar(20),
 texto varchar(600),
@@ -39,26 +39,16 @@ foreign key (fk_apelido_destinatario) references Usuario(apelido)
 );
 
 create table Tarefa(
-id_tarefa serial,
-fk_id_projeto bigint,
-/*fk_apelido_atribuicao varchar(20),*/
+id_tarefa int primary key unique,
+fk_id_projeto int,
 fk_apelido_proprietario varchar(20),
 estado varchar(30),
 titulo varchar(20),
 descricao varchar(250),
 foreign key (fk_id_projeto) references Projeto(id_projeto),
-/*foreign key (fk_apelido_atribuicao) references Usuario(apelido),*/
 foreign key (fk_apelido_proprietario) references Usuario(apelido)
 );
 
-/*
-create table Item_usuario_tarefa(
-id_item_usuario_tarefa serial,
-fk_apelido_usuario varchar(20),
-fk_id_tarefa bigint,
-foreign key (fk_apelido_usuario) references Usuario(apelido),
-foreign key(fk_id_tarefa) references Tarefa(id_tarefa)
-);
-*/
+insert into Usuario values ("Davi","davi.maicon@henrique.com","99999-9999","Bike","cemsenha","");
 
-/*drop database Jask;*/
+SELECT * FROM Usuario WHERE apelido = 'Bike';
