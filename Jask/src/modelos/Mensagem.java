9541bef54fed3e4f0769b323aa6f09215cd2b0d8
davibrandao18 <davi.brandao2013@gmail.com>
2019-11-22 13:39:08 -0300

    Atualização de classes 5.0

---
+++ Jask/src/modelos/Mensagem.java
@@ -0,0 +1,107 @@
+package modelos;
+
+/**
+ * Processa mensagem entre usuarios
+ */
+public class Mensagem
+{
+    // Composicao:
+    /**
+     * Composicao da classe Usuario
+     */
+    Usuario u = new Usuario();
+    
+    
+    // Atributos:
+    private String remetente = u.getApelido();
+    private String destinatario = u.getApelido(); // /!\ Verificar como diferenciar remetente e destinatario.
+    
+    /**
+     * id da mensagem
+     */
+    private int id;
+    /**
+     * corpo da mensagem
+     */
+    private String texto;
+    /**
+     * data e hora de envio
+     */
+    private String dhSend;
+    /**
+     * data e hora de visualizacaoo
+     */
+    private String dhView;
+    /**
+     * data e hora de recebimento
+     */
+    private String dhRecept;
+    /**
+     * estado de visualização
+     */
+    private boolean viewState;
+    
+    
+    // Construtor:
+    
+    /**
+     * @param int id
+     * @param String texto
+     * @param String dhSend
+     * @param String dhView
+     * @param String dhRecept
+     * @param boolean viewState
+     */
+    public Mensagem(int id, String texto, String dhSend, String dhView, String dhRecept, boolean viewState) {
+        this.id = id;
+        this.texto = texto;
+        this.dhSend = dhSend;
+        this.dhView = dhView;
+        this.dhRecept = dhRecept;
+        this.viewState = viewState;
+    }
+    
+    
+    // Getters e setters:
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getTexto() {
+        return this.texto;
+    }
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+    
+    public String getDhSend() {
+        return this.dhSend;
+    }
+    public void setDhSend(String dhSend) {
+        this.dhSend = dhSend;
+    }
+    
+    public String getDhView() {
+        return this.dhView;
+    }
+    public void setDhView(String dhView) {
+        this.dhView = dhView;
+    }
+    
+    public String getDhRecept() {
+        return this.dhRecept;
+    }
+    public void setDhRecept(String dhRecept) {
+        this.dhRecept = dhRecept;
+    }
+    
+    public boolean getViewState() {
+        return this.viewState;
+    }
+    public void setViewState(boolean viewState) {
+        this.viewState = viewState;
+    }  
+}
---
+++ Jask/src/modelos/Projeto.java
@@ -0,0 +1,155 @@
+
+package modelos;
+
+import java.util.ArrayList;
+
+
+/**
+ * Classe que representa um projeto
+ * 
+ * @author Davi Fonseca
+ * @version 2.0
+ * @since 1.0 -> desde qual versão esta classe está no projeto
+ *
+ */
+
+public class Projeto {
+	
+	/**
+	 * Atributo do tipo Int para armazenar o id									do Projeto.
+	 */
+	private int idProjeto;
+	
+	/**
+	 * Atributo do tipo String pra armazenar o nome								do Projeto.
+	 */
+	private String nomeProjeto;
+	
+	/**
+	 * Atributo do tipo String pra armazenar o apelido do proprietário			do Projeto
+	 */
+	private String apelidoProprietario; // /!\ --------------------------------------------> Verificar como fazer isso.
+	
+	/**
+	 * Atributo do tipo ArrayList pra armazenar os usuarios participantes		do Projeto
+	 */
+	private ArrayList<Usuario> usuarios; // /!\ --------------------------------------------> Verificar como fazer isso.
+										 // /!\ Como e um Array list não seria uma consulta apenas e nao um atributo ?
+	
+	//<---------------------------------------------------------------------------------------------------------------------------->//
+	
+	/**
+	 * Criacao de um construtor vazio:
+	 */
+	public Projeto() {}
+	
+	/**
+	 * Criacao de um contrutor cheio:
+	 * 
+	 * @param idProjeto				Armazena o ID						do Projeto
+	 * @param nomeProjeto			Armazena o nome						do Projeto
+	 * @param apelidoProprietario	Armazena o Apelido do Proprietario	do Projeto
+	 * @param usuarios				Armazena os Usuarios atrelados	 	ao Projeto
+	 */
+	public Projeto(int idProjeto, String nomeProjeto, String apelidoProprietario, ArrayList<Usuario> usuarios) {
+		this.idProjeto = idProjeto;
+		this.nomeProjeto = nomeProjeto;
+		this.usuarios = usuarios; // /!\ --------------------------------------------> Verificar como fazer isso.
+								  // /!\ Como e um Array list e possivelmente uma consulta, aqui não devia ser criado um metodo
+								  //     para exibição dos usários ?
+	}
+	
+	//<---------------------------------------------------------------------------------------------------------------------------->//
+	
+	/**
+	 * Criação de retornos de consulta	(get)
+	 *
+	 */
+	
+	/**
+	 * Retorna o ID do Projeto
+	 * @return	this.idProjeto
+	 */
+	public int getIdProjeto() {
+		return this.idProjeto;
+	}
+	
+	/**
+	 * Retorna o Nome do Projeto
+	 * @return	this.nomeProjeto
+	 */
+	public String getNomeProjeto() {
+		return this.nomeProjeto;
+	}
+
+	/**
+	 * Retorna o Apelido do Proprietario do Projeto
+	 * @return			   this.apelidoProprietario
+	 */
+	public String getApelidoProprietario() {
+		return this.apelidoProprietario;
+	}
+	
+	/**
+	 * Retorna os Usuarios atrelados ao Projeto
+	 * @return					  this.usuarios
+	 */
+	public ArrayList<Usuario> getUsuarios() {
+		return this.usuarios;
+	}
+	
+	//<---------------------------------------------------------------------------------------------------------------------------->//
+	
+	/**
+	 * Criação de metodos de alteracao	(set)
+	 *
+	 */
+	
+	/**
+	 * Altera o ID do Projeto
+	 * @param		idProjeto
+	 */
+	public void setIdProjeto(int idProjeto) {
+		this.idProjeto = idProjeto;
+	}
+	
+	/**
+	 * Altera o Nome do Projeto
+	 * @param		nomeProjeto
+	 */
+	public void setNomeProjeto(String nomeProjeto) {
+		this.nomeProjeto = nomeProjeto;
+	}
+	
+	/**
+	 * Altera o Apelido do Proprieterio do Projeto
+	 * @param 				   apelidoProprietario
+	 */
+	public void setApelidoProprietario(String apelidoProprietario) {
+		this.apelidoProprietario = apelidoProprietario;
+	}
+	
+	/**
+	 * Aletra os Usuarios participantes do projeto
+	 * @param usuarios
+	 */
+	public void setUsuarios(ArrayList<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	} // /!\ --------------------------------------------> Verificar como fazer isso.
+	  // /!\ Por ser um Arraylist não deveria ter um metódo para alterar estes registros ?.
+	
+	//<---------------------------------------------------------------------------------------------------------------------------->//
+	
+		/**
+		 * Sobescrevendo o metodo toString:
+		 */
+		@Override
+		public String toString() {
+			return "Projeto{Id Projeto: " + this.idProjeto
+					+ ", Nome: " + this.nomeProjeto
+					+ ", Proprietário: " + this.apelidoProprietario
+					+ ", Usuários: " + this.usuarios // /!\ --------------------------------------------> Verificar como fazer isso.
+				   +"}";							 // /!\ Por ser um Arraylist não deveria ter um metódo para trazer está infomação ?
+		}
+	
+}
