a2d2ba50a5a791831495d5828e91bd4c01d209cc
819160889 <819160889@PTALI30207.animaedu.intranet>
2019-11-26 18:52:35 -0300

    criação de metodo 'recuperarFoto' em 'UsuarioDao'

--- src/modelos/Principal.java
+++ src/modelos/Principal.java
@@ -13,13 +13,13 @@ public class Principal {
 		 * @author Davi Fonseca
 		 * Consultando Usuario
 		 */
-		/**UsuarioDao userDao = new UsuarioDao(Conexao.conectar());
+		UsuarioDao userDao = new UsuarioDao(Conexao.conectar());
 		
 		Usuario resultado = userDao.consultarUsuario("Bike");
 		
 		System.out.println("Apelido: " + resultado.getApelido());
 		System.out.println("Nome: " + resultado.getNome());
-		**/
+		
 		
 		//<---------------------------------------------------------------------------------------------------------------------------->//
 		
@@ -28,8 +28,11 @@ public class Principal {
 		 * Inserindo uma imagem:
 		 */
 		try {
+			//Usuario user = new Usuario("batata", "batata@ruffles.com", "4002-8922", "batBatata", "1231");
 			UsuarioDao teste = new UsuarioDao(Conexao.conectar());
-			teste.armazenarImagens(new File("github.png"), "Bike");
+			//teste.cadastrarUsuario(user);
+			teste.armazenarImagens(new File("github.png"), "Pikachu");
+			teste.recuperarImagem("Bike");
 			
 		} catch (Exception e) {
 				e.printStackTrace();	
