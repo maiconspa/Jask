8c345223bc10eea5ffec2b7a74edcd4dfe2ebbe0
davibrandao18 <davi.brandao2013@gmail.com>
2019-12-10 21:34:37 -0300

    Adição correta das imagens

--- src/modelos/Principal.java
+++ src/modelos/Principal.java
@@ -34,13 +34,14 @@ public class Principal {
 		 * Consultando Usuario
 		 */
 		
-		
+		/*
 		UsuarioDao userDao = new UsuarioDao(Conexao.conectar());
 		
 		Usuario resultadoUser = userDao.consultarUsuario("Bike");
 		
 		//LINHA DE COMANDO
-		//System.out.println("Apelido: " + resultado.getApelido());
+		System.out.println("Apelido: " + resultadoUser.getApelido());
+		*/
 		
 		//<---------------------------------------------------------------------------------------------------------------------------->//
 		
@@ -53,7 +54,8 @@ public class Principal {
 			//Usuario user = new Usuario("batata", "batata@ruffles.com", "4002-8922", "batBatata", "1231");
 			UsuarioDao teste = new UsuarioDao(Conexao.conectar());
 			//teste.cadastrarUsuario(user);
-			teste.armazenarImagens(new File("/view/logos/default2.png"), "Bike"); // /!\ - Problemas ! 
+			teste.armazenarImagens(new File("logo/default250.png"), "Bike"); // /!\ - Problemas ! 
+			System.out.println("Feito");
 			
 		} catch (Exception e) {
 			e.printStackTrace();	
